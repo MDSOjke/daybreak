a291193ed518ab4dd763d512d00a35d971312aea
Jeremy Marquis <jeremy@jeremymarquis.com>
2014-07-14 21:59:25 -0400

    prototype of index collection

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,37 @@ Y.use("node", function (Y) {
 
 	});
 });
+
+var sections = Y.all("main > section"),
+	currentSectionIndex = 0;
+
+var getSection = function (index) {
+	if (index < 0) return null;
+	var sectionList = sections.filter(":nth-child(" + (index + 1) + ")");
+	if (sectionList && sectionList.size() > 0) return sectionList.item(0);
+};
+
+var updateSections = function (force) {
+
+	var region = Y.one(window).get("viewportRegion"),
+		newSectionIndex = Math.floor(region.top / region.height);
+
+	if (newSectionIndex !== currentSectionIndex || force === true) {
+		currentSectionIndex = newSectionIndex;
+
+		var currentSection = getSection(currentSectionIndex),
+			nextSection = getSection(currentSectionIndex + 1),
+			previousSection = getSection(currentSectionIndex - 1),
+			followingSection = getSection(currentSectionIndex + 2);
+
+		if (nextSection) nextSection.toggleClass("current", true);
+		if (currentSection) currentSection.toggleClass("current", true);
+		if (previousSection) previousSection.toggleClass("current", false);
+		if (followingSection) followingSection.toggleClass("current", false);
+	}
+
+};
+
+Y.one(window).on("scroll", updateSections);
+updateSections(true);
+
